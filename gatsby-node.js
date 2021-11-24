e6e02534b00a04ffbbdaf39880cf02a2aadbfb16
Shaun Dychko <shaun@checkmarkmedia.com>
2021-11-24 14:06:52 -0800

    created katex nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,4 +11,40 @@
  *
  * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
  */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.onPreInit = () => console.log("Loaded gatsby-transformer-katex");
+
+exports.onCreateNode = ({ node, actions, reporter, createNodeId }) => {
+  if (node.internal.owner !== "gatsby-source-drupal") {
+    return {};
+  }
+
+  const node_types = ["node__page"];
+  // reporter.info(node);
+
+  if (node_types.includes(node.internal.type)) {
+    const { createNode, createParentChildLink } = actions;
+    // reporter.info(node);
+
+    let content = node.body.processed;
+    console.log(`The content: ${content}`);
+    console.log(node);
+
+    const katexNode = {
+      id: createNodeId(`${node.id} >>> Katex`),
+      children: [],
+      parent: node.id,
+      internal: {
+        contentDigest: `${node.internal.contentDigest}`,
+        type: `Katex`,
+      },
+    };
+
+    katexNode.processedKatex = `${node.body.processed} has been processed!`;
+
+    createNode(katexNode);
+    createParentChildLink({ parent: node, child: katexNode });
+    return katexNode;
+  }
+
+  return {};
+};
